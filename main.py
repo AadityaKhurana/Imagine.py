a6828334792b9e5a1cd306fbee099d3a20f9e602
Bharat Singla <58552958+singlabharat@users.noreply.github.com>
2021-08-18 20:00:39 +0530

    change some files 	new file:   __pycache__/my_functions.cpython-39.pyc 	modified:   main.py 	modified:   my_functions.py 	modified:   static/css/_utilities.scss 	modified:   static/css/index_styles.css 	modified:   static/css/index_styles.css.map 	modified:   static/css/index_styles.scss 	new file:   static/css/recreation_styles.css 	new file:   static/css/recreation_styles.css.map 	new file:   static/css/recreation_styles.scss 	deleted:    static/css/recreational_styles.css 	deleted:    static/css/recreational_styles.css.map 	deleted:    static/css/recreational_styles.scss 	modified:   templates/index.html 	modified:   templates/recreation.html

--- main.py
+++ main.py
@@ -1,37 +1,95 @@
-from flask import Flask, render_template, request
-import my_functions
-from colorama import Fore, Back, Style
+import json
+import requests
+import pandas
+import openpyxl
+import random
+import datetime
+
+num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-app = Flask(__name__)
+def get_fact():
+    response = requests.get(
+        "https://uselessfacts.jsph.pl/today.json?language=en")
+    json_response = response.json()
+    fact = json_response["text"]
+    return fact
 
-print(Fore.GREEN + 'Loading...')
-print(Style.RESET_ALL)
 
-my_fact = my_functions.get_fact()
-my_quote = my_functions.get_qoute()[0]
-author = my_functions.get_qoute()[1]
+def get_qoute():
+    current_time = datetime.datetime.now()
+    current_date = str(current_time.day) + '-' + \
+        str(current_time.month) + '-' + str(current_time.year)
+    db = pandas.read_excel('quote.xlsx')
+    my_array = db.to_numpy()
+    quote_added = bool
+    for i in range(len(my_array)):
+        if my_array[i][0] == current_date:
+            print("Quote already there")
+            quote = my_array[i][1]
+            author = my_array[i][2]
+            return_list = [quote, author]
+            quote_added = True
+        else:
+            quote_added = False
+    if quote_added == False:
+        print("Fetching...")
+        response = requests.get("https://quotes.rest/qod?language=en")
+        json_response = response.json()
+        quote = json_response["contents"]["quotes"][0]["quote"]
+        author = json_response["contents"]["quotes"][0]["author"]
+        return_list = [quote, author]
+    return return_list
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
+def get_youtube_vid():
+    response = requests.get(
+        "https://www.googleapis.com/youtube/v3/search?q=stand+up+comedy&key=AIzaSyD0VsHXytSG_QarxOHFzO9MDYMJydLT-Ag&maxResults=10")
+    json_response = response.json()
+    number = random.choice(num)
+    vid_id = json_response["items"][number]['id']["videoId"]
+    link = "https://www.youtube.com/embed/"+vid_id
+    return link
 
-@app.route('/recreation')
-def recreation():
-    link = my_functions.get_youtube_vid()
-    return render_template('recreation.html', fact = my_fact, quote = my_quote, source = link)
 
-@app.route('/reminders')
-def reminders():
-    return render_template('reminders.html')
+def reminder_check(name):
+    my_dict = dict()
+    db = pandas.read_excel('radio_btn.xlsx')
+    my_array = db.to_numpy()
+    for i in range(len(my_array)):
+        my_dict[my_array[i][0]] = my_array[i][1]
+    answer = my_dict[name]
+    return answer
 
-@app.route('/timetable')
-def timetable():
-    return render_template('timetable.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def set_reminder(name, input_value):
+    my_dict = {'water': 2, "meditate": 3,
+               "exercise": 4, "rest": 5, "entertain": 6}
+    path = "radio_btn.xlsx"
+    wb_obj = openpyxl.load_workbook(path.strip())
+    sheet_obj = wb_obj.active
+    row = my_dict[name]
+    obj_cell = sheet_obj.cell(row=row, column=2)
+    obj_cell.value = input_value
+    wb_obj.save("radio_btn.xlsx")
+
+
+def check_time_table(name):
+    my_dict_1 = dict()
+    db = pandas.read_excel('time_table.xlsx')
+    my_array = db.to_numpy()
+    for i in range(len(my_array)):
+        my_dict_1[my_array[i][0]] = my_array[i][1]
+    return my_dict_1[name]
+
+
+def set_time_table(slot, input_value):
+    my_dict = {'9-10': 2, '10-11': 3, '11-12': 4, '12-13': 5, '13-14': 6, '14-15': 7,
+               '15-16': 8, '16-17': 9, '17-18': 10, '18-19': 11, '19-20': 12, '20-21': 13}
+    path = "time_table.xlsx"
+    wb_obj = openpyxl.load_workbook(path.strip())
+    sheet_obj = wb_obj.active
+    row = my_dict[slot]
+    obj_cell = sheet_obj.cell(row=row, column=2)
+    obj_cell.value = input_value
+    wb_obj.save("time_table.xlsx")
--- my_functions.py
+++ my_functions.py
@@ -5,17 +5,21 @@ import openpyxl
 import random
 import datetime
 
-num = [1, 3, 3, 4, 5, 6, 7, 8, 9, 10]
+num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 
 def get_fact():
-    response = requests.get("https://uselessfacts.jsph.pl/today.json?language=en")
+    response = requests.get(
+        "https://uselessfacts.jsph.pl/today.json?language=en")
     json_response = response.json()
     fact = json_response["text"]
     return fact
 
+
 def get_qoute():
     current_time = datetime.datetime.now()
-    current_date = str(current_time.day)+ '-'+ str(current_time.month)+ '-'+ str(current_time.year)
+    current_date = str(current_time.day) + '-' + \
+        str(current_time.month) + '-' + str(current_time.year)
     db = pandas.read_excel('quote.xlsx')
     my_array = db.to_numpy()
     quote_added = bool
@@ -39,12 +43,14 @@ def get_qoute():
 
 
 def get_youtube_vid():
-    response = requests.get("https://www.googleapis.com/youtube/v3/search?q=stand+up+comedy&key=AIzaSyD0VsHXytSG_QarxOHFzO9MDYMJydLT-Ag&maxResults=10")
+    response = requests.get(
+        "https://www.googleapis.com/youtube/v3/search?q=stand+up+comedy&key=AIzaSyD0VsHXytSG_QarxOHFzO9MDYMJydLT-Ag&maxResults=10")
     json_response = response.json()
     number = random.choice(num)
     vid_id = json_response["items"][number]['id']["videoId"]
     link = "https://www.youtube.com/embed/"+vid_id
     return link
+
 
 def reminder_check(name):
     my_dict = dict()
@@ -55,8 +61,10 @@ def reminder_check(name):
     answer = my_dict[name]
     return answer
 
+
 def set_reminder(name, input_value):
-    my_dict = {'water': 2, "meditate": 3, "exercise": 4, "rest": 5, "entertain": 6}
+    my_dict = {'water': 2, "meditate": 3,
+               "exercise": 4, "rest": 5, "entertain": 6}
     path = "radio_btn.xlsx"
     wb_obj = openpyxl.load_workbook(path.strip())
     sheet_obj = wb_obj.active
@@ -64,6 +72,7 @@ def set_reminder(name, input_value):
     obj_cell = sheet_obj.cell(row=row, column=2)
     obj_cell.value = input_value
     wb_obj.save("radio_btn.xlsx")
+
 
 def check_time_table(name):
     my_dict_1 = dict()
@@ -73,8 +82,10 @@ def check_time_table(name):
         my_dict_1[my_array[i][0]] = my_array[i][1]
     return my_dict_1[name]
 
+
 def set_time_table(slot, input_value):
-    my_dict = {'9-10': 2, '10-11': 3, '11-12': 4, '12-13': 5, '13-14': 6, '14-15': 7, '15-16': 8, '16-17': 9, '17-18': 10, '18-19': 11, '19-20': 12, '20-21': 13}
+    my_dict = {'9-10': 2, '10-11': 3, '11-12': 4, '12-13': 5, '13-14': 6, '14-15': 7,
+               '15-16': 8, '16-17': 9, '17-18': 10, '18-19': 11, '19-20': 12, '20-21': 13}
     path = "time_table.xlsx"
     wb_obj = openpyxl.load_workbook(path.strip())
     sheet_obj = wb_obj.active
@@ -82,5 +93,3 @@ def set_time_table(slot, input_value):
     obj_cell = sheet_obj.cell(row=row, column=2)
     obj_cell.value = input_value
     wb_obj.save("time_table.xlsx")
-
-get_youtube_vid()
